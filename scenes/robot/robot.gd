02359152e7a737b7eb60687ef7d0c35f997a2aed
Sergei Kalinin <nevisdale@gmail.com>
2024-04-28 22:15:54 +0700

    scenes: add capture garbage func in robot.gd

--- scenes/robot/robot.gd
+++ scenes/robot/robot.gd
@@ -6,6 +6,11 @@ extends CharacterBody2D
 @export var _accel: float = 1200
 @export var _max_move_speed: float = 1200
 
+@onready var _capture_area: Area2D = $"CaptureArea"
+
+func _ready():
+	_capture_area.area_entered.connect(_on_area_entered)
+
 func _physics_process(delta: float) -> void:
 	_move(delta)
 
@@ -21,3 +26,13 @@ func _move(delta: float) -> void:
 		velocity += direction * _accel
 		velocity = velocity.limit_length(_max_move_speed)
 	move_and_slide()
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Garbage"):
+		_capture_garbage(area)
+	
+func _capture_garbage(garbage: Area2D) -> void:
+	var tween := create_tween().set_parallel(true)
+	tween.finished.connect(garbage.queue_free)
+	tween.tween_property(garbage, "scale", Vector2.ZERO, 0.1)
+	tween.tween_property(garbage, "global_position", global_position, 0.1)
