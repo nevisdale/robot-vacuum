fc619917f7b556965a1946a2c59fda111479c808
Sergei Kalinin <nevisdale@gmail.com>
2024-11-03 17:50:00 +0700

    fix cat and levels

--- scenes/enemies/cat.gd
+++ scenes/enemies/cat.gd
@@ -18,11 +18,11 @@ var _prev_global_position: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	if get_parent() is PathFollow2D:
-		print_debug(self, " has a PathFollow2D parent. it will be used as path to follow")
 		_path_follow = get_parent() as PathFollow2D
+	else:
+		print_debug(self, " does not have a PathFollow2D parent. do not follow the path.")
 
-	_robot_capture_area.body_entered.connect(_capture_robot)
-
+	_robot_capture_area.body_entered.connect(_try_capture_robot)
 	_prev_global_position = global_position
 
 func _process(delta: float) -> void:
@@ -30,16 +30,12 @@ func _process(delta: float) -> void:
 		_path_follow.progress += _move_speed * delta
 		# flip the Sprite2D in x axis
 		# if the Cat is moving to the left
-		const epsilon := 1
-		_sprite.flip_h = _prev_global_position.x > global_position.x + epsilon
+		_sprite.flip_h = _prev_global_position.x > global_position.x
 	_prev_global_position = global_position
 
-func _capture_robot(body: Node2D) -> void:
-	# TODO: fix it
-	# cat might capture walls or other objects
+func _try_capture_robot(body: Node2D) -> void:
 	var robot := body as Robot
-	if robot == null:
-		return
+	assert(robot != null, "The body is not a Robot.")
 
 	if not robot.can_be_captured():
 		return
