e05462e98b0d048adbd4df9a536377b25fb27623
Sergei <nevisdale@gmail.com>
2024-12-20 06:18:04 +0700

    fix garbage capturing (capturer global position in tween)

--- scripts/garbage/CollectableGarbage.cs
+++ scripts/garbage/CollectableGarbage.cs
@@ -12,12 +12,24 @@ public partial class CollectableGarbage : Area2D, IGarbage
     public bool CanBeCapturedByBin() => false;
     public bool CanBeCapturedByRobot() => true;
 
+    private Node2D _capturer;
+
+    public override void _Process(double delta)
+    {
+        if (_capturer != null)
+        {
+            // do not use in tween because global position might be changed
+            GlobalPosition = GlobalPosition.Lerp(_capturer.GlobalPosition, (float)delta * 5);
+        }
+    }
+
     public void Capture(Node2D capturer)
     {
+        _capturer = capturer;
+
         Tween tween = CreateTween();
         tween.SetParallel(true);
         tween.Finished += QueueFree;
-        tween.TweenProperty(this, "global_position", capturer.GlobalPosition, TWEEN_DURATION);
         tween.TweenProperty(this, "scale", Scale * TWEEN_SCALE_FACTOR, TWEEN_DURATION);
     }
 }
