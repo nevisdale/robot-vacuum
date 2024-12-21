661aa0c7f1b43be9cc91fd06c5e9c1979ec31187
Sergei <nevisdale@gmail.com>
2024-12-21 19:55:50 +0700

    add main menu scene

---
+++ scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+using Godot;
+using RobotVacuum.Scripts.Globals;
+
+namespace RobotVacuum.Scripts;
+
+public partial class MainMenu : CanvasLayer
+{
+    [Export(PropertyHint.File)]
+    private string _startScene = default;
+
+    private Button _play = null;
+    private Button _exit = null;
+
+    public override void _Ready()
+    {
+        _play = GetNode<Button>("%Play");
+        _exit = GetNode<Button>("%Exit");
+
+        _play.Pressed += OnPlayPressed;
+        _exit.Pressed += () => GetTree().Quit();
+    }
+
+    private void OnPlayPressed()
+    {
+        // remove all ui because transition layer does not cover it
+        GetNode("Control").QueueFree();
+        TransitionLayer.Instance.ChangeSceneTo(_startScene);
+    }
+}
